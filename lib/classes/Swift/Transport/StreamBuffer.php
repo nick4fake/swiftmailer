b6a42cc57e86a0f65c78fca9dca4471f53fd5df1
Bogdan Yurov <bogdan@yurov.me>
2015-07-21 13:39:10 +0300

    streambuffer ssl CA fix

--- lib/classes/Swift/Transport/StreamBuffer.php
+++ lib/classes/Swift/Transport/StreamBuffer.php
@@ -257,6 +257,8 @@ class Swift_Transport_StreamBuffer extends Swift_ByteStream_AbstractFilterableIn
             $timeout = $this->_params['timeout'];
         }
         $options = array();
+        $options['ssl']['verify_peer'] = FALSE;
+        $options['ssl']['verify_peer_name'] = FALSE;
         if (!empty($this->_params['sourceIp'])) {
             $options['socket']['bindto'] = $this->_params['sourceIp'].':0';
         }
